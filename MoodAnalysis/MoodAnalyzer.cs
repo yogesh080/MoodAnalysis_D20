ad749e35effeb7b9901e6484140f4a8561eef042
Yogesh <yogeshbietjhs@gmail.com>
2022-07-19 12:22:49 +0530

    Resposne happy or sad message

---
+++ MoodAnalysis/MoodAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalysis
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+
+        }
+        public string AnalizeMood()
+        {
+            if (this.message.Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+
+    }
+}
